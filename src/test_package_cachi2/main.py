ab5b69c07156fd9c941ba9d575424d286586e9a8
Ladislav Kolacek <lkolacek@redhat.com>
2023-02-06 14:42:17 +0100

    Add requests dependency
    Signed-off-by: Ladislav Kolacek <lkolacek@redhat.com>

--- src/test_package_cachi2/main.py
+++ src/test_package_cachi2/main.py
@@ -3,17 +3,16 @@ A simple script for checking pre-installed packages provided by Cachi2
 """
 
 import io
+import requests
 from dockerfile_parse import DockerfileParser
 
 
 if __name__ == '__main__':
     dfp = DockerfileParser(fileobj=io.BytesIO())
-    # Or we can try to request actual ContainerFile via requests
-    dfp.content = (
-        "FROM registry.fedoraproject.org/fedora-minimal:36\n"
-        'LABEL maintainer="Red Hat"\n'
-        "RUN pip3 install -r requirements.txt"
-    )
+    containerfile_url = ("https://raw.githubusercontent.com/containerbuildsystem/"
+                         "cachi2/main/Containerfile")
+    r = requests.get(containerfile_url, allow_redirects=True)
+    dfp.content = r.content
 
     # Print parent image
     print(dfp.parent_images[0])
